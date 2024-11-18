61ba182194c9cb58d2e3d6b06ccbaa9bb6b9dda2
rainett <96131229+rainett@users.noreply.github.com>
2024-11-18 17:00:35 +0200

    basic navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HlmInputDirective} from '@spartan-ng/ui-input-helm';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmInputDirective, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {FlightsComponent} from './flights/flights.component';
+import {LoginComponent} from './login/login.component';
+import {AccessibilityComponent} from './accessibility/accessibility.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'flights', component: FlightsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'accessibility', component: AccessibilityComponent },
+  { path: '**', redirectTo: '' },
+];
